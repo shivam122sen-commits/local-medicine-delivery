d55c42fef2853e67e6cfaccb4734e047541519aa
shivam122sen-commits <shivam122sen@gmail.com>
2026-05-22 19:47:10 +0530

    Update AuthController.java

--- backend/src/main/java/com/med/delivery/controller/AuthController.java
+++ backend/src/main/java/com/med/delivery/controller/AuthController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @CrossOrigin(origins = "*")
 public class AuthController {
 
